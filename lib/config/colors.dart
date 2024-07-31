79d43dcc7ed90f624db48df750a4a8c341abf76e
MD MAHABUB HASAN <hasanguitar4455@gmail.com>
2024-07-31 21:25:22 +0600

    dark Color

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -5,5 +5,9 @@ class AppColors {
       backgroundColor = const Color(0xFFF4F4F4),
       primaryContainerColor = const Color(0xFFFFFFFF),
       onBackgroundColor = const Color(0xFF333333),
-      onPrimaryContainerColor = const Color(0xFF333333);
+      onPrimaryContainerColor = const Color(0xFF333333),
+      darkPrimaryColor = const Color(0xFF019934),
+      darkScondaryColor = const Color(0xFF2a203e),
+      darkOnBackgroundColor = const Color(0xFFFFFFFF),
+      darkOnBGColor = const Color(0xFF212332);
 }
